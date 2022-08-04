b6ebf99d783ab1aea1c894116ce38dfaf3ee9fe8
Konstantin Antoniadi <kos-antoniadi@yandex.ru>
2022-08-04 20:45:24 +0300

    Create base app

---
+++ src/main/java/kubsauParseBot/KubsauParseBotApplication.java
@@ -0,0 +1,13 @@
+package kubsauParseBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubsauParseBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubsauParseBotApplication.class, args);
+    }
+
+}
