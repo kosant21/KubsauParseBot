fb31fadb2fe53a14eee928bc54b0ff0ac800a762
Konstantin Antoniadi <kos-antoniadi@yandex.ru>
2022-08-05 22:14:41 +0300

    create sortStringArray void

--- src/main/java/kubsauParseBot/service/TelegramBot.java
+++ src/main/java/kubsauParseBot/service/TelegramBot.java
@@ -16,13 +16,14 @@ import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.X509Certificate;
+import java.util.Arrays;
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
     final BotConfig config;
 
-    public TelegramBot(BotConfig config){
+    public TelegramBot(BotConfig config) {
         this.config = config;
     }
 
@@ -38,40 +39,74 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText()){
+        if (update.hasMessage() && update.getMessage().hasText()) {
             String messageText = update.getMessage().getText();
             long chat_id = update.getMessage().getChatId();
 
-            switch(messageText){
+            switch (messageText) {
                 case "/start":
                     //parseKubsau();
                     startCommandReceived(chat_id, parseKubsau());
                     //startCommandReceived(chat_id, update.getMessage().getChat().getFirstName());
                     break;
-                default: sendMessage(chat_id, "You're wrong");
+                default:
+                    sendMessage(chat_id, "You're wrong");
             }
 
 
         }
     }
 
-    private String parseKubsau(){
-        try{
+    private String parseKubsau() {
+
+        try {
             var document = Jsoup
-                    //connect("https://kubsau.ru/entrant/lists/")
                     .connect("https://kubsau.ru/upload/slpd/in_lists/main/139_09.04.02_000000869_%D0%94.html?0.8338041724968897")
                     .sslSocketFactory(socketFactory())
                     .get();
 
 
+            var rowElements = document.select("tr.R9");
+            rowElements.remove(0);
+            String results[][] = new String[rowElements.size()][5];
+            for (int i = 0; i < rowElements.size(); i++) {
+                var el = rowElements.get(i);
+                results[i][0] = String.valueOf(i + 1);
+                results[i][1] = el.select("td:nth-of-type(1)").text();
 
-            var titleElement = document.selectFirst("title");
-            return titleElement.text();
+                String score = el.select("td:nth-of-type(2)").text();
+                results[i][2] = score.equals("") ? "0" : score;
+
+                String agreement = el.select("td:nth-of-type(3)").text();
+                results[i][3] = agreement.equals("") ? "-" : "+";
+
+                String original = el.select("td:nth-of-type(4)").text();
+                results[i][4] = original.equals("Оригинал") ? "О" : "К";
+            }
+            results = sortStringArray(results);
+            System.out.println(Arrays.deepToString(results));
+            return rowElements.text();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         return null;
+    }
+
+    private String[][] sortStringArray(String mas[][]) {
+        for (int i = 0; i < mas.length; i++) {
+            for (int j = 0; j < mas.length - 1; j++) {
+                if (Integer.parseInt(mas[j][2]) < Integer.parseInt(mas[j + 1][2])) {
+                    var temp = mas[j];
+                    mas[j] = mas[j + 1];
+                    mas[j + 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < mas.length; i++) {
+            mas[i][0] = String.valueOf(i + 1);
+        }
+        return mas;
     }
 
     private SSLSocketFactory socketFactory() {
@@ -96,20 +131,20 @@ public class TelegramBot extends TelegramLongPollingBot {
         }
     }
 
-    private void startCommandReceived(long chatId, String name){
+    private void startCommandReceived(long chatId, String name) {
         String answer = "Hi, " + name + ", nice to meet you!";
 
         sendMessage(chatId, answer);
     }
 
-    private void sendMessage(long chatId, String textToSend){
+    private void sendMessage(long chatId, String textToSend) {
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
         message.setText(textToSend);
 
-        try{
+        try {
             execute(message);
-        }catch (TelegramApiException e){
+        } catch (TelegramApiException e) {
             e.printStackTrace();
         }
     }
